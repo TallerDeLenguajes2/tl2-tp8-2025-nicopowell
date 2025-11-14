914565741c3d243c9ed8f4e8309e822982d64e90
nicopowell <nicopowell30@gmail.com>
2025-11-13 22:52:22 -0300

    se agrego el crud de productos

--- Controllers/ProductosController.cs
+++ Controllers/ProductosController.cs
@@ -15,4 +15,45 @@ public class ProductosController: Controller
         List<Producto> productos = _productoRepository.GetAll();
         return View(productos);
     }
+
+    [HttpGet]
+    public IActionResult Create()
+    {
+        return View();
+    }
+
+    [HttpPost]
+    public IActionResult Create(Producto producto)
+    {
+        _productoRepository.Create(producto);
+        return RedirectToAction("Index");
+    }
+
+    [HttpGet]
+    public IActionResult Edit(int id)
+    {
+        Producto producto = _productoRepository.GetById(id);
+        return View(producto);
+    }
+
+    [HttpPost]
+    public IActionResult Edit(int id, Producto productoEditado)
+    {
+        _productoRepository.Update(id, productoEditado);
+        return RedirectToAction("Index");
+    }
+
+    [HttpGet]
+    public IActionResult Delete(int id)
+    {
+        Producto producto = _productoRepository.GetById(id);
+        return View(producto);
+    }
+
+    [HttpPost, ActionName("Delete")]
+    public IActionResult DeleteConfirmado(int id)
+    {
+        _productoRepository.DeleteById(id);
+        return RedirectToAction("Index");
+    }
 }
